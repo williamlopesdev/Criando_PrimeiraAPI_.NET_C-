d008683728064da96f7f2e489df5937b954e460e
William Lopes Da Silva Malaquias <william.malaquias@funcionalcorp.com.br>
2021-05-06 22:26:45 -0300

    master

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using ContosoPets.Api;
+using ContosoPets.Api.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ContosoPets.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args) =>
+            CreateHostBuilder(args).Build().SeedDatabase().Run();
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+    }
+
+    public static class IHostExtensions
+    {
+        public static IHost SeedDatabase(this IHost host)
+        {
+            var scopeFactory = host.Services.GetRequiredService<IServiceScopeFactory>();
+            using var scope = scopeFactory.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<ContosoPetsContext>();
+
+            if (context.Database.EnsureCreated())
+                SeedData.Initialize(context);
+
+            return host;
+        }
+    }    
+}
